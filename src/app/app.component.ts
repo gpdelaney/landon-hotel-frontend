cd3032bb64c1c2046bf3d3987179505a13fe5d43
Will Delaney <will@pop-os.localdomain>
2020-05-19 12:41:04 +0200

    First commit with hardcoded data and static files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reservation-app';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  private baseUrl = 'http://localhost:8080';
+  private reservationUrl = this.baseUrl + '/room/v1/reservation/';
+  rooms: Room[] = [];
+
+  ngOnInit(): void {
+    this.rooms = [new Room('127', '127', '150'), new Room('128', '128', '150'), new Room('129', '129', '150')];
+  }
+}
+
+export class Room {
+  constructor(id: string, roomNumber: string, price: string) {
+    this.id = id;
+    this.roomNumber = roomNumber;
+    this.price = price;
+  }
+
+  id: string;
+  roomNumber: string;
+  price: string;
 }
